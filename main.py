39514b38397c097953d4444ac1e633cd8431220a
Thomas Breloff <tom@ybit.co>
2012-08-01 21:48:53 -0400

    x

--- main.py
+++ main.py
@@ -6,34 +6,72 @@ class Canvas(QGraphicsScene):
     def __init__(self):
         super(Canvas,self).__init__()
 
-        plane = Sprite()
-        self.addItem(plane)
+        self.setSceneRect(0, 0, 1000, 1000)
 
+        self.plane = Sprite()
+        self.addItem(self.plane)
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.update)
+        timer.start(100)
+
+    def update(self):
+        self.plane.move()
+        for item in self.items():
+            item.move()
+
+    
     def keyPressEvent(self, e):
-        print 'pressed', e.key()
+        print 'press'
         if e.key() == Qt.Key_Up:
-            print 'press up'
-            plane.up(True)
+            self.plane.dirsPressed[0] = True
+        elif e.key() == Qt.Key_Down:
+            self.plane.dirsPressed[1] = True
+        elif e.key() == Qt.Key_Left:
+            self.plane.dirsPressed[2] = True
+        elif e.key() == Qt.Key_Right:
+            self.plane.dirsPressed[3] = True
 
     def keyReleaseEvent(self, e):
-        print 'released', e.key()
+        print 'rel'
         if e.key() == Qt.Key_Up:
-            print 'rel up'
-            plane.up(False)
+            self.plane.dirsPressed[0] = False
+        elif e.key() == Qt.Key_Down:
+            self.plane.dirsPressed[1] = False
+        elif e.key() == Qt.Key_Left:
+            self.plane.dirsPressed[2] = False
+        elif e.key() == Qt.Key_Right:
+            self.plane.dirsPressed[3] = False
 
 class Sprite(QGraphicsPixmapItem):
     def __init__(self):
         super(Sprite,self).__init__()
-        self.upPressed = False
+        self.dirsPressed = [False for i in xrange(4)]
         self.x = 50
         self.y = 100
-        self.setOffset(self.x, self.y)
+        print self.x, self.y
+        #self.setPos(self.x, self.y)
 
-    def up(self, isPressed):
-        self.upPressed = isPressed
+        self.pix = QPixmap("sprites/Multiple Views/F-22B.PNG")
+        self.setPixmap(self.pix)
+        print self.pix, self.pix.isNull(), self.pix.load("sprites/Multiple Views/F-22B.PNG")
+        print self.pix.width()
 
-    
-        
+    def move(self):
+        if self.dirsPressed[0]:
+            self.y -= 1
+        if self.dirsPressed[1]:
+            self.y += 1
+        if self.dirsPressed[2]:
+            self.x -= 1
+        if self.dirsPressed[3]:
+            self.x += 1
+        self.setPos(self.x, self.y)
+
+    #def paint(self, painter, option, widget):
+    #    print 'paint'
+    #    super(Sprite,self).paint(painter, option, widget)
+
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
