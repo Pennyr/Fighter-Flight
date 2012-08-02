1098d050dc4a39ac012b03c2d0f2efb27f06c9f6
Thomas Breloff <tom@ybit.co>
2012-08-01 23:24:01 -0400

    enemies need work

--- main.py
+++ main.py
@@ -8,21 +8,61 @@ class Canvas(QGraphicsScene):
 
         self.setSceneRect(0, 0, 1000, 1000)
 
-        self.plane = Sprite()
+        self.plane = Plane()
         self.addItem(self.plane)
+
+        self.bullets = []
+        self.fire_on = False
+        self.fire_cnt = 0
+
+        self.enemies = []
+        [self.addEnemy(i*50) for i in xrange(1)]
 
         timer = QTimer(self)
         timer.timeout.connect(self.update)
-        timer.start(100)
+        timer.start(10)
+
+    def addEnemy(self, x = 50, y = 100):
+        enemy = Enemy(x, y)
+        self.enemies.append(enemy)
+        self.addItem(enemy)
 
     def update(self):
         self.plane.move()
-        for item in self.items():
-            item.move()
 
+        todel = []
+        for i,bullet in enumerate(self.bullets):
+            bullet.move()
+            if bullet.y < 0:
+                print 'removing', i, 'out of', len(self.bullets), bullet.y
+                self.removeItem(bullet)
+                todel.append(bullet)
+
+        self.bullets = [b for b in self.bullets if b.y >= 0]
+        for b in todel:
+            del b
+
+        for enemy in self.enemies:
+            enemy.move()
     
+        if self.fire_on:
+            if self.fire_cnt >= 5:
+                #print 'FIRE'
+                bullet = Bullet(self.plane.x + self.plane.width / 2, self.plane.y)
+                self.addItem(bullet)
+                self.bullets.append(bullet)
+                self.fire_cnt = 0
+            else:
+                self.fire_cnt += 1
+
     def keyPressEvent(self, e):
-        print 'press'
+        if e.isAutoRepeat():
+            return
+
+        if e.key() == Qt.Key_Space:
+            self.fire_on = True
+            self.fire_cnt = 5
+        
         if e.key() == Qt.Key_Up:
             self.plane.dirsPressed[0] = True
         elif e.key() == Qt.Key_Down:
@@ -33,7 +73,12 @@ class Canvas(QGraphicsScene):
             self.plane.dirsPressed[3] = True
 
     def keyReleaseEvent(self, e):
-        print 'rel'
+        if e.isAutoRepeat():
+            return
+        
+        if e.key() == Qt.Key_Space:
+            self.fire_on = False
+
         if e.key() == Qt.Key_Up:
             self.plane.dirsPressed[0] = False
         elif e.key() == Qt.Key_Down:
@@ -44,33 +89,80 @@ class Canvas(QGraphicsScene):
             self.plane.dirsPressed[3] = False
 
 class Sprite(QGraphicsPixmapItem):
-    def __init__(self):
+    def __init__(self, x = 50, y = 100):
         super(Sprite,self).__init__()
-        self.dirsPressed = [False for i in xrange(4)]
-        self.x = 50
-        self.y = 100
-        print self.x, self.y
-        #self.setPos(self.x, self.y)
+        self.x = x
+        self.y = y
 
-        self.pix = QPixmap("sprites/Multiple Views/F-22B.PNG")
-        self.setPixmap(self.pix)
-        print self.pix, self.pix.isNull(), self.pix.load("sprites/Multiple Views/F-22B.PNG")
-        print self.pix.width()
+    def on_move(self):
+        pass
 
     def move(self):
+        self.on_move()
+        self.setPos(self.x, self.y)
+        
+class Bullet(Sprite):
+    def __init__(self, x = 50, y = 100):
+        super(Bullet,self).__init__(x,y)
+        
+        pix = QPixmap("sprites/Multiple Views/F-22B.PNG").copy(0,0,3,3)
+        self.setPixmap(pix)
+    
+    def on_move(self):
+        self.y -= 5
+
+class Enemy(Sprite):
+    def __init__(self, x = 50, y = 100):
+        super(Enemy,self).__init__(x,y)
+    
+        pix = QPixmap("sprites/Multiple Views/MiG-X3.PNG")
+        self.setPixmap(pix)
+
+
+class Plane(Sprite):
+    def __init__(self, x = 50, y = 100):
+        super(Plane,self).__init__(x,y)
+
+        self.dirsPressed = [False for i in xrange(4)]
+        self.cnt = [0,0]
+
+        pix = QPixmap("sprites/Multiple Views/F-22B.PNG")
+        t = QTransform ().rotate(-90)
+        pix = pix.transformed(t)
+        w = pix.width() / 3
+        self.width = w
+        self.pixs = [pix.copy(i * w, 0, w, pix.height()) for i in xrange(3)]
+        t = QTransform().rotate(-180,Qt.XAxis)
+        self.pixs.extend([QPixmap.fromImage(pix.toImage().mirrored(True, False)) for pix in self.pixs[1:]])
+        self.setPixmap(self.pixs[0])
+
+    def on_move(self):
+        # y axis
         if self.dirsPressed[0]:
             self.y -= 1
-        if self.dirsPressed[1]:
+        elif self.dirsPressed[1]:
             self.y += 1
+
+        # x axis
         if self.dirsPressed[2]:
             self.x -= 1
-        if self.dirsPressed[3]:
+            self.cnt[0] += 1
+            self.cnt[1] = 0
+            if self.cnt[0] < 50:
+                self.setPixmap(self.pixs[1])
+            else:
+                self.setPixmap(self.pixs[2])
+        elif self.dirsPressed[3]:
             self.x += 1
-        self.setPos(self.x, self.y)
-
-    #def paint(self, painter, option, widget):
-    #    print 'paint'
-    #    super(Sprite,self).paint(painter, option, widget)
+            self.cnt[1] += 1
+            self.cnt[0] = 0
+            if self.cnt[1] < 50:
+                self.setPixmap(self.pixs[3])
+            else:
+                self.setPixmap(self.pixs[4])
+        else:
+            self.cnt = [0,0]
+            self.setPixmap(self.pixs[0])
 
     
 if __name__ == '__main__':
